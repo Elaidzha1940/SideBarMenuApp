c9003133afc0a9a7ed71098d95d4cd245c2e91bd
Elaidzha1940 <eli080293@gmail.com>
2024-07-28 20:19:05 +0300

    Added UserImage

--- SideBarMenu/ContentView.swift
+++ SideBarMenu/ContentView.swift
@@ -17,21 +17,8 @@ struct ContentView: View {
                 .mask(RoundedRectangle(cornerRadius: 15, style: .continuous))
             
             VStack {
-                HStack {
-                    Circle()
-                        .frame(width: 65, height: 65)
-                        .foregroundStyle(.white)
-                    
-                    VStack {
-                        RoundedRectangle(cornerRadius: 3, style: .continuous)
-                            .frame(width: 65, height: 14)
-                        RoundedRectangle(cornerRadius: 3, style: .continuous)
-                            .frame(width: 65, height: 14)
-                        RoundedRectangle(cornerRadius: 3, style: .continuous)
-                            .frame(width: 65, height: 14)
-
-                    }
-                }
+                UserImage()
+                Spacer()
             }
         }
         .ignoresSafeArea()
@@ -42,4 +29,27 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct UserImage: View {
+    var body: some View {
+        HStack {
+            Circle()
+                .frame(width: 65, height: 65)
+                .foregroundStyle(.white)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                RoundedRectangle(cornerRadius: 3, style: .continuous)
+                    .frame(width: 100, height: 14)
+                RoundedRectangle(cornerRadius: 3, style: .continuous)
+                    .frame(width: 75, height: 7)
+                    .opacity(0.5)
+                RoundedRectangle(cornerRadius: 3, style: .continuous)
+                    .frame(width: 52, height: 7)
+                    .opacity(0.5)
+            }
+        }
+        .foregroundStyle(.white)
+        .padding(.top, 65)
+    }
 }
